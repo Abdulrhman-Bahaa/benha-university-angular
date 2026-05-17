f61839de7c30ce190f07c31d73116d9146571c09
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2026-05-17 14:36:17 +0300

    Auto-generated commit

--- chunk-UKC6AG35.js
+++ chunk-UKC6AG35.js
@@ -1,4 +1,4 @@
-import{e as h}from"./chunk-HPJS4YWN.js";import{S as i,X as s,Za as e}from"./chunk-EDUXOEBP.js";var o=(()=>{class c{constructor(l){this.sanitizer=l,this._categories=e([{id:1,name:"Sport",icon:this.sanitizer.bypassSecurityTrustHtml(`<svg
+import{e as h}from"./chunk-FIH6REAI.js";import{S as i,X as s,_a as e}from"./chunk-JOVNFIMP.js";var o=(()=>{class c{constructor(l){this.sanitizer=l,this._categories=e([{id:1,name:"Sport",icon:this.sanitizer.bypassSecurityTrustHtml(`<svg
               xmlns="http://www.w3.org/2000/svg"
               width="55"
               height="62"
